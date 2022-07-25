f2c24235b818950960987e3a6038f298042585e4
Vanessa Milliken <vanessa.milliken@everquote.com>
2022-07-25 12:07:05 +0100

    logoutadded

---
+++ src/components/LogoutButton.js
@@ -0,0 +1,18 @@
+// src/components/LogoutButton.js
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+function LogoutButton() {
+ const {
+   isAuthenticated,
+   logout,
+   } = useAuth0();
+
+   return isAuthenticated && (
+     <button onClick={() => {
+     logout({ returnTo: window.location.origin });
+    }}>Log out</button>
+ );
+};
+
+export default LogoutButton;
